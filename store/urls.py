a42cef54b897918a91b4df36d8365dbfa93187a5
rayhan <rayhunkhan27@gmail.com>
2020-10-28 15:16:16 +0600

    alternative way to actime middleware

--- store/urls.py
+++ store/urls.py
@@ -6,13 +6,15 @@ from .views.login import logout
 from .views.cart import Cart
 from .views.chackout import Chackout
 from .views.order import OrderView
+from django.utils.decorators import method_decorator
 
+from store.middlewares.auth import auth_middleware
 urlpatterns = [
     path('', Index.as_view(), name='home'),
     path('signup/', Signup.as_view(), name='signup'),
     path('login/', Login.as_view(), name='login'),
     path('logout/', logout, name='logout'),
-    path('cart/', Cart.as_view(), name='cart'),
-    path('chackout/', Chackout.as_view(), name='chackout'),
-    path('orders/', OrderView.as_view(), name='orders'),
+    path('cart/', auth_middleware(Cart.as_view()), name='cart'),
+    path('chackout/', auth_middleware(Chackout.as_view()), name='chackout'),
+    path('orders/',auth_middleware(OrderView.as_view()), name='orders'),
 ]
