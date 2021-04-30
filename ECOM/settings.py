7b05ae468c1bec43446b85c791847f8b59991c1e
rayhun <rayhunkhan27@gmail.com>
2021-05-01 01:03:17 +0600

    update file

--- ECOM/settings.py
+++ ECOM/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,5 +135,5 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/product_image/'
 MEDIA_ROOT =os.path.join(BASE_DIR, 'static/images')
 
-
+django_heroku.settings(locals())
 
---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn ECOM.wsgi --log-file -
